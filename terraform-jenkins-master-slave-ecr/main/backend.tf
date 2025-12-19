09c52b48d2613ce74adb3e8de57b940ba2f86cd5
Ritesh Kumar Singh <56765895+singhritesh85@users.noreply.github.com>
2025-12-19 17:55:22 +0530

    Delete terraform-jenkins-master-slave-ecr directory

--- terraform-jenkins-master-slave-ecr/main/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
- backend "s3" {
-   bucket         = "dolo-dempo"
-   key            = "state/jenkins/terraform.tfstate"
-   region         = "us-east-2"
-   encrypt        = true
-   use_lockfile   = true   ###dynamodb_table = "terraform-state"
- }
-}
--- terraform-jenkins-master-slave-ecr/main/output.tf
+++
@@ -1,4 +0,0 @@
-output "ECR_URI_jenkins_master_and_slave_ec2_private_ip_and_alb_dns_name" {
-  description = "Details of the ECR URI, Jenkins-Master, Jenkins-Slave Private IP and Jenkins ALB DNS Name"
-  value       = "${module.jenkins}"
-}
