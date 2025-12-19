604d673abeba4feb470873a1e292e5aff893bea3
Ritesh Kumar Singh <56765895+singhritesh85@users.noreply.github.com>
2025-12-19 12:56:19 +0530

    Delete terraform-ecs-task-definition-service-ecr-alb directory

--- terraform-ecs-task-definition-service-ecr-alb/main/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
- backend "s3" {
-   bucket         = "dolo-dempo"
-   key            = "state/ecs/terraform.tfstate"
-   region         = "us-east-2"
-   encrypt        = true
-   use_lockfile   = true   ###dynamodb_table = "terraform-state"
- }
-}
